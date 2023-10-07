5a1362f3425e2e81be51597e0f1ec7aa161299c8
serdroi <145987898+serdroi@users.noreply.github.com>
2023-10-07 20:32:42 +0200

    Create tracking.php

---
+++ tracking.php
@@ -0,0 +1,11 @@
+<?php
+date_default_timezone_set('Europe/Kiev');
+$date=date('ymdHis',time());
+$fndate=date('ymd',time());
+$filecache = "tracking/tr$fndate.log";
+$fhc=fopen($filecache,"a+"); 
+flock($fhc,2);
+$success = fputs($fhc, $date."|".getenv("REMOTE_ADDR")."|".$_SERVER['HTTP_REFERER']."|".getenv("SCRIPT_NAME")."|".getenv("QUERY_STRING")."|".getenv("HTTP_USER_AGENT")."\n\r");
+flock($fhc,3);
+fclose($fhc);
+?>
